6a13f14651278478e81189f20713d3e3d8d5f9ed
dayaman <s16t279@stu.kagawa-u.ac.jp>
2020-08-23 13:49:18 +0900

    募集チャンネルを限定

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import model
 import dsctoken
 
 client = discord.Client()
-spot = ['試練', 'スカイフック', '調査キャンプ', '製錬所', 'カウントダウン', 'エピセンター', 'フラグメント', '列車庫', '溶岩湖', 'ハーベスター', '仕分け工場', 'ラバシティー', '火力発電所', 'ザ・ツリー', 'ザ・ドーム', '展望', '発射場', 'ブラストウォール', '間欠泉']
+spot = ['試練', 'スカイフック', '調査キャンプ', '製錬所', 'カウントダウン', 'エピセンター', 'フラグメント', '列車庫', '溶岩湖', 'ハーベスター', '仕分け工場', 'ラバシティー', '火力発電所', 'ザ・ツリー', 'ザ・ドーム', '展望', '発射場', '間欠泉']
 
 @client.event
 async def on_message(message):
@@ -12,6 +12,9 @@ async def on_message(message):
         return
 
     if not message.role_mentions:
+        return
+
+    if not str(message.channel.id) == model.get_recuit(str(message.guild.id)).channel_id:
         return
     
     overwrites = {
@@ -26,7 +29,7 @@ async def on_message(message):
     tx_ch = await ctgr.create_text_channel(name='チャット')
     await ctgr.create_voice_channel(name='わいわい')
 
-    model.add(str(message.guild.id), str(ctgr.id))
+    model.add_category(str(ctgr.id))
 
     await message.channel.send('{}を作りました。'.format(tx_ch.mention), delete_after=30)
     await check(message.guild, ctgr)
@@ -63,5 +66,21 @@ async def check(guild, ex_ctgr):
                 if not vc.members:
                     await delete_channel(ctgr)
 
+@client.event
+async def on_guild_join(guild):
+    # 募集部屋がなければ募集部屋作成
+    recuit_ch = model.get_recuit(str(guild.id))
+    if recuit_ch is None:
+        ctgr = await guild.create_category('room-creator')
+        tx_ch = await ctgr.create_text_channel(name='ぼしゅー')
+
+        model.add_recuit(str(guild.id), str(tx_ch.id))
+    else:
+        ch = guild.get_channel(int(recuit_ch.channel_id))
+        if ch is None:
+            ctgr = await guild.create_category('room-creator')
+            tx_ch = await ctgr.create_text_channel(name='ぼしゅー')
+
+            model.change_recuit(str(guild.id), str(tx_ch.id))
 
 client.run(dsctoken.tkn)
--- model.py
+++ model.py
@@ -9,10 +9,15 @@ session=SessionClass()
 Base = declarative_base()
 
 class Category(Base):
-    __tablename__ = "category"
+    __tablename__ = 'category'
     category_id = Column(String, primary_key=True)
 
-def add(guild_id, category_id):
+class Recuitment(Base):
+    __tablename__ = 'recuitment'
+    channel_id = Column(String)
+    guild_id = Column(String, primary_key=True)
+
+def add_category(category_id):
     ctgr = Category(category_id=category_id)
     session.add(ctgr)
     session.commit()
@@ -25,6 +30,21 @@ def delete_category(category_id):
     ctgr = session.query(Category).filter(Category.category_id == category_id).one()
     session.delete(ctgr)
     session.commit()
+
+def add_recuit(guild_id, channel_id):
+    recuit = Recuitment(channel_id=channel_id, guild_id=guild_id)
+    session.add(recuit)
+    session.commit()
+
+def get_recuit(guild_id):
+    recuit = session.query(Recuitment).filter(Recuitment.guild_id == guild_id).one_or_none()
+    return recuit
+
+def change_recuit(guild_id, channel_id):
+    recuit = session.query(Recuitment).filter(Recuitment.guild_id == guild_id).one_or_none()
+    if recuit is None:
+        return recuit
+    recuit.channel_id = channel_id
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
